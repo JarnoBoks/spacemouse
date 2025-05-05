5c31bdc96650c766d5e0b09c59922980a615df21
Jarno Boks <jarno.boks@gmail.com>
2025-05-05 11:15:06 +0200

    Fix typo

--- src/visitors/IPrinterVisitor.h
+++ src/visitors/IPrinterVisitor.h
@@ -1,5 +1,5 @@
 #pragma once
-// This Visitor interfacre is used to implement the Visitor pattern for the Sensor and Hardware classes.
+// This Visitor interfacee is used to implement the Visitor pattern for the Sensor and Hardware classes.
 // It allows for different types of visitors to be applied to the Sensor and Hardware classes without modifying their code.
 
 // We have to add all the classes that we want to visit here

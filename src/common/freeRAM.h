a9a2f92132aebb47239fe3285a8ea6a03d16bee6
Jarno Boks <jarno.boks@gmail.com>
2025-05-17 09:13:00 +0200

    Added Freeram for ESP32

--- src/common/freeRAM.h
+++ src/common/freeRAM.h
@@ -1,28 +1,40 @@
 #pragma once
 #include <Arduino.h>
 
-// NOTE - This code is only for AVR architecture.
-
 class FreeRAM {
 public:
+#if defined(ARDUINO_ARCH_AVR) || defined(ARDUINO_ARCH_ESP32)
     static void display_freeram() {
-#ifdef ARDUINO_ARCH_AVR
         Serial.print(F("- SRAM left: "));
         Serial.println(freeRam());
-#endif
     }
+#else
+    static void display_freeram() {
+        Serial.println(F("Free RAM not available on this architecture."));
+    }
+#endif
 
+#if defined(ARDUINO_ARCH_AVR)
     static int freeRam() {
-#ifdef ARDUINO_ARCH_AVR
         extern int __heap_start, *__brkval;
         int v;
         return (int)&v - (__brkval == 0
                               ? (int)&__heap_start
                               : (int)__brkval);
-#else
         return 0; // Return 0 for non-AVR architectures
-#endif
     }
+
+#else
+#if defined(ARDUINO_ARCH_ESP32)
+    static int freeRam() {
+        return heap_caps_get_free_size(MALLOC_CAP_8BIT);
+
+#else
+    static int freeRam() {
+        return 0; // Return 0 for non-AVR architectures
+    }
+#endif // ARDUINO_ARCH_ESP32
+#endif // ARDUINO_ARCH_AVR
 
     FreeRAM() = default;  // Default constructor
     ~FreeRAM() = default; // Default destructor
--- src/observers/DebugOutput/DebugOutputLoopFrequency.hpp
+++ src/observers/DebugOutput/DebugOutputLoopFrequency.hpp
@@ -9,28 +9,27 @@
  */
 class DebugOutputLoopFrequency : public DebugOutput {
 private:
-    unsigned long _lastFrequencyUpdate; // time from millis(), when the last debug output was given
-    int _iterationsPerSecond = 0;       // number of loop iterations per second
+    unsigned long m_lastFrequencyUpdate; // time from millis(), when the last debug output was given
+    int m_iterationsPerSecond = 0;       // number of loop iterations per second
 public:
-    DebugOutputLoopFrequency() : _lastFrequencyUpdate(millis()) {
+    DebugOutputLoopFrequency() : m_lastFrequencyUpdate(millis()) {
         Serial.println(F("..."));
     };
     virtual ~DebugOutputLoopFrequency() {}; // nothing to do in destructor
 
     void update(IObservable *axisCollection) override {
-        _iterationsPerSecond++;
-        if (millis() - _lastFrequencyUpdate > 1000) { // if one second has past: report frequency
+        m_iterationsPerSecond++;
+        if (millis() - m_lastFrequencyUpdate > 1000) { // if one second has past: report frequency
             Serial.print(F("Freq: "));
-            Serial.print(_iterationsPerSecond);
+            Serial.print(m_iterationsPerSecond);
             Serial.print(F(" Hz"));
 
-            // Free RAM is only available for AVR architecture
             Serial.print(F(", Free RAM: "));
             Serial.print(FreeRAM::freeRam());
             Serial.println(F(" bytes"));
 
-            _lastFrequencyUpdate = millis(); // reset timer
-            _iterationsPerSecond = 0;        // reset iteration counter
+            m_lastFrequencyUpdate = millis(); // reset timer
+            m_iterationsPerSecond = 0;        // reset iteration counter
         }
     }
 };

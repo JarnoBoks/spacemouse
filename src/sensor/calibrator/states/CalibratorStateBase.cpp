e2ee6b82ec868c4ab2cef53ca215711b2eb08cd6
Jarno Boks <jarno.boks@gmail.com>
2025-06-12 00:35:16 +0200

    Renamed observer member

--- src/sensor/calibrator/states/CalibratorStateBase.cpp
+++ src/sensor/calibrator/states/CalibratorStateBase.cpp
@@ -3,13 +3,13 @@
 #include <sensor/SensorCollection.hpp> // Include the SensorCollection class header file
 
 CalibratorStateBase::~CalibratorStateBase() {
-    if (!sensorObserver)
+    if (!m_sensorObserver)
         return;
 
     SensorCollection *sensorCollection = context->getSensorCollection(); // Get the sensor collection from the context
     if (sensorCollection)
-        sensorCollection->detachObserver(sensorObserver); // Detach the observer from the sensor collection
+        sensorCollection->detachObserver(m_sensorObserver); // Detach the observer from the sensor collection
 
-    delete sensorObserver;    // Delete the observer to free memory
-    sensorObserver = nullptr; // Set the observer pointer to null
+    delete m_sensorObserver;    // Delete the observer to free memory
+    m_sensorObserver = nullptr; // Set the observer pointer to null
 }
--- src/sensor/calibrator/states/CalibratorStateBase.hpp
+++ src/sensor/calibrator/states/CalibratorStateBase.hpp
@@ -9,7 +9,7 @@ class CalibratorStateBase : public ICalibratorState {
     // Base class for all calibrator states, providing common functionality and interface
     // for managing the calibration process of the sensors in the SpaceMouse.
 protected:
-    IObserver *sensorObserver = nullptr;      // Pointer to the (sensor) observer associated with this state
+    IObserver *m_sensorObserver = nullptr;    // Pointer to the (sensor) observer associated with this state
     unsigned long m_startCalibrationTime = 0; // Calibration start time from millis()
 public:
     CalibratorStateBase() = default; // Default constructor

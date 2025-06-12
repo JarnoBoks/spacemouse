216f58999d524cbd65acde344bd81d9294cb1d79
Jarno Boks <jarno.boks@gmail.com>
2025-06-12 08:22:22 +0200

    Added comments to sensor.hpp

--- src/sensor/sensors/Sensor.hpp
+++ src/sensor/sensors/Sensor.hpp
@@ -10,6 +10,9 @@ class SensorConfig; // Forward declaration of SensorConfig class
  * @brief   Base class representing a sensor, implementing the ICollectable interface.
  * @details This class is a base class for different types of sensors. It provides common functionality for reading and processing sensor values.
  *          The idleposition and the deadzone are calculated on startup by the Idle calibration process.
+ *          The sensor value is read from the analog-to-digital converter (ADC) and processed to obtain the final value.
+ *          The class also provides methods for setting and getting the sensor's configuration, idle position, deadzone, and final value.
+ *          SensorConfiguration is managed through the SensorConfig class, which holds the minimum and maximum raw values and the inversion state.
  * @note    The Sensor class is designed to be inherited by specific sensor types, such as HallSensor or JoystickSensor.
  *          It provides a common interface for reading and processing sensor values, as well as managing the sensor's configuration.
  */
@@ -43,24 +46,34 @@ public:
     /// @brief Retrieve the configuration object of the Sensor.
     inline SensorConfig *getConfig() const { return config; };
 
+    /// @brief Retrieve the idle position of the sensor.
     inline int getIdlePosition() const { return m_idleposition; }
     virtual bool setIdlePosition(int val);
     virtual bool idlePositionOk(const int val) const = 0; // Pure virtual function to be implemented by derived sensor classes     //REFACTOR - Remove parameter, like the deadzoneWarning function
 
+    /// @brief Retrieve the deadzone of the sensor.
     inline uint8_t getDeadzone() const { return m_deadzone; }
     virtual bool setDeadzone(const uint8_t dz);
     virtual bool isDeadzoneOk() const = 0; // Pure virtual function to be implemented by derived sensor classes
 
-    /// @brief Retrieve the raw sensor value, the value as read from the AD converter.
+    /// @brief Retrieve the raw sensor value
+    /// @return The raw sensor value as read from the AD converter.
     inline int getRawValue() const { return m_rawValue; }
 
-    /// @brief Retrieve the centered sensor value, the value after the IdlePosition offset is applied.
+    /// @brief Retrieve the centered sensor value.
+    /// @details This value is the raw sensor value adjusted by the idle position.
     inline int getCntValue() const { return m_cntValue; }
 
-    /// @brief Retrieve the final sensor value, the value after deadzone correction and mapping is applied.
+    /// @brief Retrieve the final sensor value.
+    /// @details This value is the centered sensor value adjusted by the deadzone and mapping.
     inline int getFinValue() const { return m_finValue; }
 
+    /// @brief Retrieve the descriptor of the sensor.
+    /// @details The descriptor is a string that describes the sensor, used for identification and logging.
     inline const char *getDescriptor() const { return descriptor; }
+
+    /// @brief Retrieve the pin number of the sensor.
+    /// @details The pin number is used to identify the physical pin on the microcontroller to which the sensor is connected.
     inline const uint8_t getId() const { return static_cast<uint8_t>(id); };
 
     virtual void setContext(ICollection *Collection) override {};
